a80688f8e6dff154fbc1856cb460f1163f743bae
Dustin Thomson <dthomson@51systems.com>
2016-11-24 21:22:26 -0700

    Removed Assetic dependencies

--- src/Module.php
+++ src/Module.php
@@ -2,21 +2,16 @@
 
 namespace Dext;
 
-use Zend\EventManager\EventInterface;
-use Zend\ModuleManager\Feature\BootstrapListenerInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\ModuleManager\Feature\InitProviderInterface;
-use Zend\ModuleManager\ModuleManager;
 use Zend\ModuleManager\ModuleManagerInterface;
-use Zend\Mvc\MvcEvent;
 
 /**
  *
  */
 class Module implements
     ConfigProviderInterface,
-    InitProviderInterface,
-    BootstrapListenerInterface
+    InitProviderInterface
 {
 
     public function getConfig()
@@ -30,47 +25,5 @@ class Module implements
     public function init(ModuleManagerInterface $moduleManager)
     {
         $moduleManager->loadModule('Dojo');
-        $moduleManager->loadModule('AsseticBundle');
-    }
-
-    /**
-     * @param MvcEvent|EventInterface $e
-     * @return array|void
-     */
-    public function onBootstrap(EventInterface $e)
-    {
-        $serviceManager = $e->getApplication()->getServiceManager();
-
-        /** @var $as \AsseticBundle\Service */
-        $as = $serviceManager->get('AsseticService');
-
-        //Register the dext javascript files with assetic
-        $assetManager = $as->getAssetManager();
-
-        $this->_registerDextAssets($assetManager);
-
-        /** @var $view \Zend\View\RendererInterface */
-        $view = $serviceManager->get('viewmanager')->getRenderer();
-
-        /**
-         * @var $dojo \Dojo\Builder\Configuration
-         * Note that this is actually a \Dojo\View\Helper\Dojo object that we proxy to configuration.
-         */
-        $dojo = $view->plugin('dojo');
-        $baseUrl = rtrim($as->getConfiguration()->getBaseUrl() . $as->getConfiguration()->getBasePath(), '/');
-
-        $dojo->getDojoConfig()->registerPackage('dext', $baseUrl . '/js/dext');
-    }
-
-    /**
-     * Registers all files in the asset directory with assetic.
-     *
-     * @param \Assetic\AssetManager $am
-     */
-    private function _registerDextAssets(\Assetic\AssetManager $am)
-    {
-        $rootDir = __DIR__ . DIRECTORY_SEPARATOR . 'assets';
-
-        AsseticHelper::registerDir($am, $rootDir);
     }
 }
